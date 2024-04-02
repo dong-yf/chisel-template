6bf38af11bd70e3fe7d20d8961ea32ef02fc00c4
dong-yf <2959032105@qq.com>
2024-04-02 14:54:50 +0800

    add  RocketChip Multiplier

---
+++ src/test/scala/MulFormalSpec.scala
@@ -0,0 +1,13 @@
+package example.rocketchip
+
+import chisel3._
+import chiseltest._
+import chiseltest.formal._
+import org.scalatest.flatspec.AnyFlatSpec
+
+class MulFormalSpec extends AnyFlatSpec with Formal with ChiselScalatestTester {
+    behavior of "Multiplier"
+    it should "pass" in {
+        verify(new Mul(8), Seq(BoundedCheck(20), Z3EngineAnnotation))
+    }
+}
