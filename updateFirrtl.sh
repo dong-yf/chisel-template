0598d6b4b8642ad0c16ece3961695e07190188e8
dong-yf <2959032105@qq.com>
2024-08-30 11:22:47 +0800

    add script

--- updateFirrtl.sh
+++ updateFirrtl.sh
@@ -1,6 +1,17 @@
+set -e
 cd firrtl
 sbt "publishLocal"
 cd ..
+sbt "testOnly example.Example112FormalSpec"
+# sbt "testOnly nutcore.CacheFormalSpec"
+# sbt "testOnly nutcore.CacheStage3FormalSpec" 
+# sbt "testOnly newtemplate.Template01nFormalSpec"
+# sbt "testOnly examples.ArbiterFormalSpec"
+# sbt "testOnly example.rocketchip.DivFormalSpec"
+# sbt "testOnly example.rocketchip.MulFormalSpec"
+# sbt "testOnly template.Template31FormalSpec"
+# sbt "testOnly examples.heappriorityqueue.modules.LinearSearchMemFormalSpec"
 # sbt "testOnly gcd.DecoupledGCDFormalSpec"
-sbt "testOnly testCHA.GCD.DecoupledGcdSpec"
+# sbt "testOnly formal.NutCoreFormalSpec"
 # sbt "testOnly example.xiangshan.DividerFormalSpec"
+# sbt "testOnly mini.RISCVMiNiFormalSpec"
